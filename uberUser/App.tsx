8d95d35ddb2f2bbc35177be16e11385e792a3774
AndreSDS <andre_smiths@outlook.com>
2023-04-13 16:23:11 -0300

    setup amplify and authentication

--- uberUser/App.tsx
+++ uberUser/App.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./src/navigation";
 
-export default function App() {
+import { Amplify } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig)
+
+function App() {
   return (
     <NavigationContainer>
       <StatusBar style="light" translucent backgroundColor="#ffffff20" />
@@ -11,3 +16,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App);
